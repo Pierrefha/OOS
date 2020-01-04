6352895b95b6d72db084f8bc3f2abe146d5f338d
Pierre <pierre.dahmani@alumni.fh-aachen.de>
2020-01-04 19:58:13 +0100

    renamed project

---
+++ oosPraktikum5_Aufgabe1/src/application/AnmeldungsController.java
@@ -0,0 +1,56 @@
+/*
+ * javafx application Paket, in dem sich um die Darstellung gekümmert wird
+ */
+package application;
+
+import javafx.application.Platform;
+import javafx.scene.control.*;
+import benutzerVerwaltung.*;
+/*
+ * Klasse die für die Logik unserer Anmeldung Scene verantwortlich ist.
+ * @author xddq
+ */
+public class AnmeldungsController {
+		
+	//attribute of type MainApplication used for callback
+	public MainApplication application;
+
+	public TextField userID;
+	public PasswordField passwordFirst;
+	public PasswordField passwordSecond;
+	
+	/*
+	 * Methode um die Referenz zur MainApplication zu setzen.
+	 * param application: Referenz zur MainApplication
+	 */
+	void setMainApplication(MainApplication application) {
+		this.application = application;
+	}
+	
+	/*
+	 * Methode um den Text des TextFelds zu setzen
+	 * param caption: Text der Dargestellt werden soll
+	 */
+	void setCaptionOfTextfield(String caption) {
+		userID.setText(caption);
+	}
+	
+	
+	/*
+	 * method to create a user with our given input 
+	 * and display it's string value
+	 */
+	public void buttonClicked() {
+		//compare if both passwords match
+		if(passwordFirst.getText().equals(passwordSecond.getText())) {
+			//create userObj and print it and close scene
+			Benutzer newUser = new Benutzer(userID.getText(),passwordFirst.getText().toCharArray());
+			System.out.println(newUser.toString());
+			application.neuerBenutzer(newUser);
+		}
+		else {
+			userID.setText("Passwort != Wiederholung");
+		}
+	}
+	
+}
---
+++ oosPraktikum5_Aufgabe1/src/benutzerVerwaltung/BenutzerVerwaltung.java
@@ -0,0 +1,26 @@
+/**
+ * Paket zum dritten Praktikum in OOS.
+ */
+package benutzerVerwaltung;
+
+/**
+ * Interface das Schnittstelle zur Benutzerverwaltung zur Verfügung stellt.
+ * @author xddq
+ *
+ */
+public interface BenutzerVerwaltung {
+
+	/*
+	 * Methode um einen Benutzer im System einzutragen.
+	 * @param benutzer: Objekt eines Benutzers, der im System eingetragen werden soll.
+	 */
+	public void benutzerEintragen(Benutzer benutzer) throws UserAlreadyExistsException,UnsafePasswordException,UsernameToShortException;
+	
+	/*
+	 * Methode um zu überprüfen, ob Benutzer im System bereits existiert.
+	 * @param benutzer: Objekt eines Benutzers, der überprüft werden soll.
+	 * @return true wenn der Benutzer existiert, sonst false.
+	 */
+	public boolean benutzerOK(Benutzer benutzer);
+
+}
