6342779460ac29d95c627ba7c09e72600a16ec61
Pierre <pierre.dahmani@alumni.fh-aachen.de>
2020-01-29 19:36:03 +0100

    add adapter pattern

---
+++ oosPraktikum5/oos_adapter_pattern/src/oos_adapter_pattern/CluelessIdiotTarget.java
@@ -0,0 +1,11 @@
+package oos_adapter_pattern;
+
+/*
+ * this is what the client is actually calling
+ */
+public interface CluelessIdiotTarget {
+	
+	public String theLongYellowSweetFruity();
+	public double wasLesdePrais();
+	public String theGreenRoundDoctorAwayKeeper();
+}
