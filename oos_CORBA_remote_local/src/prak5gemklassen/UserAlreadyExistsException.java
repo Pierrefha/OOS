34740411a1e7d322fa33556b7eb6adbc8a343571
Pierre <pierre.dahmani@alumni.fh-aachen.de>
2020-02-07 19:00:21 +0100

    upload old exams and add sample from 2019

---
+++ oos_CORBA_remote_local/src/prak5gemklassen/UserAlreadyExistsException.java
@@ -0,0 +1,23 @@
+/**
+ * Paket zum dritten Praktikum in OOS.
+ */
+package prak5gemklassen;
+
+/**
+ * Exception die ausgelöst wird, wenn man versucht einen bereits bestehenden
+ * Benutzer erneut im System zu registrieren.
+ * @author xddq
+ *
+ */
+public class UserAlreadyExistsException extends Exception {
+
+	/*
+	 * Konstruktor der Exception
+	 * param ausgabe: Auszugebener String der Fehlermeldung.
+	 */
+	public UserAlreadyExistsException(String ausgabe) {
+		super(ausgabe);
+	}
+	
+	
+}
